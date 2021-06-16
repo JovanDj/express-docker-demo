809ba58906a8b267bd5e95f73b72fc98df26ef87
Jovan <JovanDj@users.noreply.github.com>
2021-06-16 15:22:36 +0200

    Users crud

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(cors());
 
